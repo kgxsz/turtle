bb3b7ac0e185783cddbfd304e070e12d474e1de7
Keigo Suzukawa <k.suzukawa@gmail.com>
2018-12-18 20:22:18 +0000

    Tighten the call to the ticker API

--- src/clj/turtle/handler.clj
+++ src/clj/turtle/handler.clj
@@ -31,13 +31,12 @@
                                         :apikey (System/getenv "ALPHA_VANTAGE_API_KEY")}}
         {:keys [body]} (client/get "https://www.alphavantage.co/query" request-options)]
     ;; TODO - error handling
-    {:ticker (->> (get (cheshire/parse-string body) "Time Series (Daily)")
-                  (map (fn [[k v]] {:instant (time.coerce/to-long k)
-                                    :open (-> v (get "1. open") (Double.))
-                                    :close (-> v (get  "5. adjusted close") (Double.))}))
-                  (sort-by :instant <)
-                  (take 10)
-                  (vec))}))
+    {:ticker (mapv
+              (fn [[k v]]
+                {:instant (time.coerce/to-long k)
+                 :open (-> v (get "1. open") (Double.))
+                 :close (-> v (get  "5. adjusted close") (Double.))})
+              (get (cheshire/parse-string body) "Time Series (Daily)"))}))
 
 (defmethod handle-query :default [query]
   (throw (Exception.)))
